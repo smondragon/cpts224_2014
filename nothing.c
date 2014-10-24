3773f9384e2ceca9b0624dec5f2889f0e8fadb86
Evan Felix <karcaw@gmail.com>
2014-10-23 21:29:59 -0700

    cleanup indents

--- nothing.c
+++ nothing.c
@@ -14,83 +14,82 @@
 int main(int argc, char *argv[], char *envp[])
 {
 
-    unsigned long i,j,s;
+    unsigned long i, j, s;
     unsigned long count;
-    unsigned long multiplier=1000000;
-    double d=0.0;
-    double d2=0.0;
-    
+    unsigned long multiplier = 1000000;
+    double d = 0.0;
+    double d2 = 0.0;
+
     if (argc > 1)
-        multiplier *= strtoul(argv[1],NULL,10);
+	multiplier *= strtoul(argv[1], NULL, 10);
 
     printf("Begin Code\n");
 
-    for (i=0;i<multiplier;i++)
-        d = sin(d+(double)i/multiplier);
+    for (i = 0; i < multiplier; i++)
+	d = sin(d + (double) i / multiplier);
 
-	STEP;
+    STEP;
 
-    for (i=0; i<multiplier; i++)
-        d = (i%3 == 0 ? multiplier++ : multiplier);
+    for (i = 0; i < multiplier; i++)
+	d = (i % 3 == 0 ? multiplier++ : multiplier);
 
-	STEP;
+    STEP;
 
     //Daniel Hanlen
-    for(i=0;i<multiplier;i++)
-        for(j=0;j<i;j=j+(multiplier/1000))
-            d=(double)(i-j)/(double)(i+j);
+    for (i = 0; i < multiplier; i++)
+	for (j = 0; j < i; j = j + (multiplier / 1000))
+	    d = (double) (i - j) / (double) (i + j);
 
-	STEP;
+    STEP;
 
-   //r Jacqueline Wilkes
+    //r Jacqueline Wilkes
 
 
-    for(i=0;i<multiplier;i++)
-        d += multiplier;
+    for (i = 0; i < multiplier; i++)
+	d += multiplier;
     STEP;
 //Eric Thomas
-    for (i=0; i<multiplier; i++)
-    {
-        d = multiplier * multiplier;
-        d = d - multiplier;
+    for (i = 0; i < multiplier; i++) {
+	d = multiplier * multiplier;
+	d = d - multiplier;
     }
     STEP;
-    
+
     // Jorge Diaz
-    for (count=0; count < multiplier; count++)
-        d2 = sin(d2+(double)count/multiplier);
+    for (count = 0; count < multiplier; count++)
+	d2 = sin(d2 + (double) count / multiplier);
     STEP;
 
     //Rebecca Hoerner
-    d=0.0;
-    for (i=0;i<multiplier;i++)
-        d = cos(d * (double)i/multiplier) * 2.0;
+    d = 0.0;
+    for (i = 0; i < multiplier; i++)
+	d = cos(d * (double) i / multiplier) * 2.0;
     STEP;
 
-        /*
-         * By Michael Wilkins
-         */
-        for (i=0; i < multiplier; i++)
-                d = (multiplier-i)/(multiplier+i);
+    /*
+     * By Michael Wilkins
+     */
+    for (i = 0; i < multiplier; i++)
+	d = (multiplier - i) / (multiplier + i);
 
-        STEP;
-	// Sandra M
-	for(s=0;s<multiplier;s++)
-		d = sin(d+(double)(s*0.05)/multiplier);
-	STEP;
+    STEP;
+    // Sandra M
+    for (s = 0; s < multiplier; s++)
+	d = sin(d + (double) (s * 0.05) / multiplier);
+    STEP;
 
-	//Devlyn
-	for (i=0;i<multiplier;i++)
-       		d = sin(d+(double)(i*3.066)/multiplier);
+    //Devlyn
+    for (i = 0; i < multiplier; i++)
+	d = sin(d + (double) (i * 3.066) / multiplier);
 
-	STEP;
-	// Emilio Orozco
-	for(i=0,i<multiplier;i++;)
-		d=cos(d+(double)(i*2)/multiplier);
+    STEP;
+    // Emilio Orozco
+    for (i = 0, i < multiplier; i++;)
+	d = cos(d + (double) (i * 2) / multiplier);
 
-	STEP;
-	// By Nick Bunn
-	for (i=0;i<multiplier; i++)
-		d = (multiplier * 2) / (multiplier - 2);
-	STEP;
+    STEP;
+    // By Nick Bunn
+    for (i = 0; i < multiplier; i++)
+	d = (multiplier * 2) / (multiplier - 2);
+    STEP;
 }
