0d1fa8ebbf08428b5750b821be335dde9552b56e
SandraMondragon <sandra.mondragon@tricity.wsu.edu>
2014-11-03 15:40:31 -0800

    Sandy

--- nothing.c
+++ nothing.c
@@ -9,72 +9,87 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define STEP printf("Done with step %d\n",__COUNTER__);
 
 int main(int argc, char *argv[], char *envp[])
 {
 
-    unsigned long i,j,s;
+    unsigned long i, j, s;
     unsigned long count;
-    unsigned long multiplier=1000000;
-    double d=0.0;
-    double d2=0.0;
-    
+    unsigned long multiplier = 1000000;
+    double d = 0.0;
+    double d2 = 0.0;
+
     if (argc > 1)
-        multiplier *= strtoul(argv[1],NULL,10);
+	multiplier *= strtoul(argv[1], NULL, 10);
 
     printf("Begin Code\n");
 
-    for (i=0;i<multiplier;i++)
-        d = sin(d+(double)i/multiplier);
+    for (i = 0; i < multiplier; i++)
+	d = sin(d + (double) i / multiplier);
 
-    printf("Done with Step 1\n");
+    STEP;
 
-    for (i=0; i<multiplier; i++)
-        d = (i%3 == 0 ? multiplier++ : multiplier);
+    for (i = 0; i < multiplier; i++)
+	d = (i % 3 == 0 ? multiplier++ : multiplier);
 
-    printf("Done with Step 2\n"); // Steven ahl
+    STEP;
 
     //Daniel Hanlen
-    for(i=0;i<multiplier;i++)
-        for(j=0;j<i;j=j+(multiplier/1000))
-            d=(double)(i-j)/(double)(i+j);
-    printf("Done with Step Dan.\n");
+    for (i = 0; i < multiplier; i++)
+	for (j = 0; j < i; j = j + (multiplier / 1000))
+	    d = (double) (i - j) / (double) (i + j);
+
+    STEP;
+
+    //r Jacqueline Wilkes
 
 
-    // Jacqueline Wilkes
-    for(i=0;i<multiplier;i++)
-        d += multiplier;
-    printf("Done with step 2.5");
+    for (i = 0; i < multiplier; i++)
+	d += multiplier;
+    STEP;
 //Eric Thomas
-    for (i=0; i<multiplier; i++)
-    {
-        d = multiplier * multiplier;
-        d = d - multiplier;
-
-
+    for (i = 0; i < multiplier; i++) {
+	d = multiplier * multiplier;
+	d = d - multiplier;
     }
-    printf("Done with step 3");
-    
+    STEP;
+
     // Jorge Diaz
-    for (count=0; count < multiplier; count++)
-        d2 = sin(d2+(double)count/multiplier);
-    printf("Done with Step 2\n");
+    for (count = 0; count < multiplier; count++)
+	d2 = sin(d2 + (double) count / multiplier);
+    STEP;
 
     //Rebecca Hoerner
-    d=0.0;
-    for (i=0;i<multiplier;i++)
-        d = cos(d * (double)i/multiplier) * 2.0;
-    printf("Done with Step 4\n");
+    d = 0.0;
+    for (i = 0; i < multiplier; i++)
+	d = cos(d * (double) i / multiplier) * 2.0;
+    STEP;
 
-        /*
-         * By Michael Wilkins
-         */
-        for (i=0; i < multiplier; i++)
-                d = (multiplier-i)/(multiplier+i);
+    /*
+     * By Michael Wilkins
+     */
+    for (i = 0; i < multiplier; i++)
+	d = (multiplier - i) / (multiplier + i);
 
-        printf("Completed Step 5\n");
-	// Sandra M
-	for(s=0;s<multiplier;s++)
-		d = sin(d+(double)(s*0.05)/multiplier);
-	printf("Done with step 6!!\n");
+    STEP;
+    // Sandra M
+    for (s = 0; s < multiplier; s++)
+	d = sin(d + (double) (s * 0.05) / multiplier);
+    STEP;
+
+    //Devlyn
+    for (i = 0; i < multiplier; i++)
+	d = sin(d + (double) (i * 3.066) / multiplier);
+
+    STEP;
+    // Emilio Orozco
+    for (i = 0, i < multiplier; i++;)
+	d = cos(d + (double) (i * 2) / multiplier);
+
+    STEP;
+    // By Nick Bunn
+    for (i = 0; i < multiplier; i++)
+	d = (multiplier * 2) / (multiplier - 2);
+    STEP;
 }
